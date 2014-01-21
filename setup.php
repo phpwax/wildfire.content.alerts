5115bad6005fef14097ba571483fd7b5159ac397
Charles Marshall <charles@oneblackbear.com>
2014-01-21 16:55:57 +0000

    remove that reference

--- setup.php
+++ setup.php
@@ -1,7 +1,6 @@
 <?
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
-AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
 AutoLoader::$plugin_array[] = array("name"=>"wildfire.notifications","dir"=>__DIR__);
 
 CMSApplication::register_module("notification", array("display_name"=>"Notifications", "link"=>"/admin/notification/"));
